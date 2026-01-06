07754a82be54c127a096031288283aeef0ef72aa
Hayden Barogh <hbarogh@gmail.com>
2026-01-05 19:48:53 -0600

    Initial commit

---
+++ somnianalytics/somnianalytics/somnianalyticsApp.swift
@@ -0,0 +1,17 @@
+//
+//  somnianalyticsApp.swift
+//  somnianalytics
+//
+//  Created by Hayden Barogh on 1/5/26.
+//
+
+import SwiftUI
+
+@main
+struct somnianalyticsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
