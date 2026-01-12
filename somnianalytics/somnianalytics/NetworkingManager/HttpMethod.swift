6e31720f9662fab59785531259418f777126c55f
Hayden Barogh <hbarogh@gmail.com>
2026-01-12 15:35:44 -0600

    Started making the http networking client

---
+++ somnianalytics/somnianalytics/NetworkingManager/HttpMethod.swift
@@ -0,0 +1,18 @@
+//
+//  HttpMethod.swift
+//  somnianalytics
+//
+//  Created by Hayden Barogh on 1/12/26.
+//
+
+import Foundation
+
+//This just represents the http methods that are supported by the networking manager, 
+enum HttpMethods: String {
+    case get = "GET"
+    case post = "POST"
+    case put = "PUT"
+    case delete = "DELETE"
+}
+
+
