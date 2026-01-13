be76d31b901a9bd16389edad74f1c39f3e10e0bb
Hayden Barogh <hbarogh@gmail.com>
2026-01-13 12:14:10 -0600

    Added the error descriptions to the network errors.

--- somnianalytics/somnianalytics/NetworkingManager/ApiError.swift
+++ somnianalytics/somnianalytics/NetworkingManager/ApiError.swift
@@ -15,6 +15,20 @@ enum NetworkErrors: Error {
     case decodingError(Error)
 }
 
+extension NetworkErrors: LocalizedError {
+    var errorDescription: String? {
+        switch self {
+        case .invalidURL:
+            return NSLocalizedString("Invalid URL (400): Unable to perform request for the given url.", comment: "invalidURL Error")
+        case .invalidResponse:
+            return NSLocalizedString("Invalid Response.", comment: "invalidResponse")
+        case .errorResponse(let errorResponse):
+            return NSLocalizedString("Error \(errorResponse.message ?? "")", comment: "Error Response")
+        case .decodingError(let error):
+            return NSLocalizedString("Unable to decode successfully.Error: \(error)", comment: "decodingError")
+        }
+    }
+}
 
 struct ErrorResponse: Codable {
     let message: String?
