ef3dd2ee8d5e1b455574f9e5a017ff5e6ae6a5b5
Hayden Barogh <hbarogh@gmail.com>
2026-01-13 15:17:58 -0600

    Just added a quick comment.

--- somnianalytics/somnianalytics/Authentication/TokenProvider.swift
+++ somnianalytics/somnianalytics/Authentication/TokenProvider.swift
@@ -7,14 +7,14 @@
 
 import Foundation
 
-
+//This file is built to handle the JWT token management for the app
 protocol TokenProviderProtocol {
     func getToken() -> String?
     func clearToken() -> Void
     func setToken(_ token: String) -> Void
 }
 
-// TODO: Need to finish making this 
+// TODO: Need to finish making this
 struct TokenProvider: TokenProviderProtocol {
     func getToken() -> String? {
         <#code#>
