73e6f25ce3330f06716e576ec120630aa1977c68
Hayden Barogh <hbarogh@gmail.com>
2026-02-23 13:12:58 -0600

    scheme for xcode

---
+++ somnianalytics/somnianalytics/Views/Main/AlarmView.swift
@@ -0,0 +1,18 @@
+//
+//  AlarmView.swift
+//  somnianalytics
+//
+//  Created by Hayden Barogh on 2/23/26.
+//
+
+import SwiftUI
+
+struct AlarmView: View {
+    var body: some View {
+        Text("Alarm View")
+    }
+}
+
+#Preview {
+    AlarmView()
+}
---
+++ somnianalytics/somnianalytics/Views/Main/DetailsView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailsView.swift
+//  somnianalytics
+//
+//  Created by Hayden Barogh on 2/23/26.
+//
+
+import SwiftUI
+
+struct DetailsView: View {
+    var body: some View {
+        Text("Details View")
+    }
+}
+
+#Preview {
+    DetailsView()
+}
