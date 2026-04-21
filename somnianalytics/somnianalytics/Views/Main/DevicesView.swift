d94c5ec22a960cf1f94ef36acf3d293f407fa7f0
Bryan Zavala <noexprtees@gmail.com>
2026-04-21 15:40:25 -0500

    feature: add some more information to the devices screen

--- somnianalytics/somnianalytics/Views/Main/DevicesView.swift
+++ somnianalytics/somnianalytics/Views/Main/DevicesView.swift
@@ -7,15 +7,44 @@ struct DevicesView: View {
     
     var body: some View {
         VStack(alignment: .leading) {
-            Text("Available Devices")
-                .font(.system(size: 24))
-            List(ble.discovered) { device in
-                HStack(alignment: .center) {
-                    Text(device.name)
-                    Spacer()
-                    Button("Connect", action: {})
+            if let somnitrix = ble.connected {
+                Text("Looks like you're already connected to a Somnitrix device. You are good to go then!")
+                    .font(.subheadline)
+                    .background(Color.gray.opacity(0.5))
+                    .multilineTextAlignment(.center)
+                InfoItem(title: "Device Name", value: somnitrix.name)
+                InfoItem(title: "Device UUID", value: somnitrix.id.uuidString)
+            } else {
+                Text("Like you're not connected to a Somnitrix device. Please choose from one of the below devices and connec to it.")
+                    .font(.subheadline)
+                    .background(Color.gray.opacity(0.5))
+                    .multilineTextAlignment(.center)
+                if ble.discovered.isEmpty {
+                    Text("No devices found. Make sure Bluetooth is powered on and try again!")
+                } else {
+                    List(ble.discovered) { device in
+                        HStack(alignment: .center) {
+                            Text(device.name)
+                            Spacer()
+                            Button("Connect", action: {})
+                        }
+                    }
                 }
             }
         }
+        .navigationTitle("Devices")
+        .navigationBarTitleDisplayMode(.large)
     }
+    
+    private func InfoItem(title: String, value: String) -> some View {
+        HStack {
+            Text(title)
+            Spacer()
+            Text(value)
+        }
+    }
+}
+
+#Preview {
+    DevicesView()
 }
