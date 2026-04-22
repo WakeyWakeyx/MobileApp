64b58addcfb53fee339509f78214ced27d01fda1
Bryan Zavala <noexprtees@gmail.com>
2026-04-22 16:11:59 -0500

    refactor: slight changes to devices view

--- somnianalytics/somnianalytics/Views/Main/DevicesView.swift
+++ somnianalytics/somnianalytics/Views/Main/DevicesView.swift
@@ -6,43 +6,43 @@ struct DevicesView: View {
     @Environment(SomniManager.self) private var ble: SomniManager
     
     var body: some View {
-        VStack(alignment: .leading) {
-            if let somnitrix = ble.connected {
-                Text("Looks like you're already connected to a Somnitrix device. You are good to go then!")
-                    .font(.subheadline)
-                    .background(Color.gray.opacity(0.5))
-                    .multilineTextAlignment(.center)
-                InfoItem(title: "Device Name", value: somnitrix.name)
-                InfoItem(title: "Device UUID", value: somnitrix.id.uuidString)
-                Button("Disconnect", action: { ble.disconnect() })
+        VStack(alignment: .leading, spacing: 15) {
+            Text("This is where you can configure connections to a Somnitrix device. If you have previously connected to your Somnitrix device, you can see its details here. If you haven't connected to a Somnitrix device yet, you can select one from the list below.")
+                .font(.subheadline)
+                .padding(10)
+                .background(RoundedRectangle(cornerRadius: 15).fill(.gray.opacity(0.25)))
+            if let device = ble.connected {
+                Section("About Your Somnitrix") {
+                    LabeledString(title: "Device Name", value: device.name)
+                    LabeledString(title: "Device UUID", value: device.id.uuidString)
+                    Button("Disconnect", action: { ble.disconnect() })
+                }
+            } else if ble.discovered.isEmpty {
+                Text("No devices found. Make sure your Somnitrix device is powered on.")
             } else {
-                Text("Like you're not connected to a Somnitrix device. Please choose from one of the below devices and connec to it.")
-                    .font(.subheadline)
-                    .background(Color.gray.opacity(0.5))
-                    .multilineTextAlignment(.center)
-                if ble.discovered.isEmpty {
-                    Text("No devices found. Make sure Bluetooth is powered on and try again!")
-                } else {
-                    List(ble.discovered) { device in
-                        HStack(alignment: .center) {
+                List(ble.discovered) { device in
+                    LabeledContent(
+                        content: {
+                            Button("Connect", action: { ble.connect(device: device) })
+                        },
+                        label: {
                             Text(device.name)
-                            Spacer()
-                            Button("Connect", action: {})
                         }
-                    }
+                    )
                 }
             }
+            Spacer()
         }
+        .padding(10)
         .navigationTitle("Devices")
         .navigationBarTitleDisplayMode(.large)
     }
     
-    private func InfoItem(title: String, value: String) -> some View {
-        HStack {
-            Text(title)
-            Spacer()
-            Text(value)
-        }
+    private func LabeledString(title: String, value: String) -> some View {
+        LabeledContent(
+            content: { Text(value) },
+            label: { Text(title) }
+        )
     }
 }
 
